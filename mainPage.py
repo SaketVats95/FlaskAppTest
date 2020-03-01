28a903bc3f4f4bd0dafc6bcdd6bc37b4753d69e4
Saket Vats <44232561+SaketVats95@users.noreply.github.com>
2020-03-01 20:43:47 +0530

    readData method introduced

--- mainPage.py
+++ mainPage.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    #if True: 
+    return ''.join(readData('file1.txt'))
     return "Hello, World!"
 
+def readData(fileName,startLine=0,endLine=0):
+    lines=[]
+    with open('./data/'+fileName,'r') as f:
+        lines=f.read().split('\n')
+    #print(lines) 
+    if(startLine!=0 and endLine!=0):
+        return lines[startLine:endLine]
+    return lines
+
 if __name__ == "__main__":
+    readData('file1.txt')
     app.run(debug=True)
 
 
