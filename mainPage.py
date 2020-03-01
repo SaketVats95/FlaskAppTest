412dcfab866d510d745adedf568ff3ae91b4ba5f
Saket Vats <44232561+SaketVats95@users.noreply.github.com>
2020-03-01 21:30:56 +0530

    added condition for startLineNumber and endLineNumber

--- mainPage.py
+++ mainPage.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
+#http://10.1.1.1:5000/file1?startLineNumber=10&endLineNumber=20
 @app.route('/',defaults={'fileName': 'file1'})
 @app.route('/<fileName>')
 def index(fileName):
     #if True:
+    startLine,endLine = 0 , 0  
+    try:
+        startLine = int(request.args.get('startLineNumber'))
+        endLine = int(request.args.get('endLineNumber'))
+    except Exception as e:
+        print('start and end not provided by user')
     fileName=fileName+'.txt' 
     return ''.join(readData(fileName))
     return "Hello, World!"
@@ -16,6 +23,8 @@ def readData(fileName,startLine=0,endLine=0):
         lines=f.read()
     print(lines) 
     if(startLine!=0 and endLine!=0):
+        if(startLine<0 or endLine>len(lines)):
+            return "Provided start Line number or end Line Number is not present in {0}".format(fileName)
         return lines[startLine:endLine]
     return lines
 
